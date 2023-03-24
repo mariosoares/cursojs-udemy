5e8de67b7274566e20ce8cb4b18461ccfaa565aa
mariosoares <mario.soares@gmail.com>
2023-03-24 09:35:43 -0300

    primeiro commit

---
+++ secao2/exercicio.js
@@ -0,0 +1,2 @@
+console.log('Meu nome é "Mario". Estou aprendendo Javascript às 10 da manhã.')
+console.log('Meu nome é "Mario". Estou aprendendo Javascript às',10, 'da manhã.')
---
+++ secao2/index.js
@@ -0,0 +1,8 @@
+
+console.log('Curso Javascript');
+console.log("Curso Javascript");
+console.log(`Curso Javascript`);
+console.log("'Curso Javascript'");
+console.log(`'Curso' "Javascript"`);
+
+console.log(35, 15.85, "Curso Javascript");
