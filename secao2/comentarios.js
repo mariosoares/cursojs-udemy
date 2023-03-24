96c6c6abbfa914d6057c4301720b34779da13fb9
mariosoares <mario.soares@gmail.com>
2023-03-24 19:25:35 -0300

    segundo commit, na pr/pe

---
+++ secao2/comentarios.js
@@ -0,0 +1,18 @@
+// Escrever um comentário de linha
+console.log("Olá mundo!"); //Aqui tem outro comentário
+console.log("Lnha 3");
+console.log("Lnha 4");
+console.log("Lnha 5");
+console.log("Lnha 6");
+// Vai ser ignorado
+// O node utiliza a mesma engine, o mesmo motor do google chrome
+
+/* Comentário de bloco.
+Comentando
+múltiplas linhas
+*/
+
+/**
+ * 
+ * 
+ */
