716e9bf15689711c06caec5a7bbdd2dcaa409bc5
mariosoares <mario.soares@gmail.com>
2023-04-18 18:03:44 -0300

    commit 18-04-2023

--- secao3-js-log-prog/63-tratando-e-lancado-erros-try-catch-throw.js
+++ secao3-js-log-prog/63-tratando-e-lancado-erros-try-catch-throw.js
@@ -3,27 +3,38 @@
 // apresentar erro, retorna o valor de catch
 
 try {
-    console.log(naoExisto); // tenta código
+    console.log(naoExisto); // try -> tente executar o código
 }
-catch (err) {
+
+catch (err) { // poderia ser qualquer nome, "e", "erro", etc
     console.log('naoExisto não existe'); //executa algum outro código informado se apresentar erro
-    //console.log(err); 
+    //console.log(err); //msg de erro original com stack trace, sem o tratamento
     // salva o código em caso de erro, na variável informada "err" para ser tratado, etc
 } // a msg de erro original não deve ser exibida ao usuário por questão de segurança e/ou organização
+console.log();
 
 function soma(x, y) {
     if (typeof x !== 'number' || typeof y !== 'number') {
-        throw new Error('x e y precisam ser números') ; // gerei o erro referente a não atingir a condição do if
+        throw new Error ('x e y precisam ser números') ; // lancei o erro, gerei o erro referente a atingir a condição do if
     }
-    return x + y}
+    return x + y};
 
+console.log(soma(1, 2));
+//console.log(soma('1', 2)); //aqui, sem o try catch, é lançado meu próprio erro criado com throw
 console.log();
 
-try {
+try { // tratando o erro criado por mim
     console.log(soma(1, 2));
     console.log(soma('1', 2));
 } catch (erro) {
-    // console.log(erro)
+    console.log(erro)
+}
+console.log();
+
+try { // tratando o erro criado por mim
+    console.log(soma(1, 2));
+    console.log(soma('1', 2));
+} catch (erro) {
     console.log('## Solicitação apresentou erro durante execução ##');
     //console.log(error);
     //console.log('Alguma coisa mais amigável pro usuário.');
---
+++ secao3-js-log-prog/64-tratando-erros-try-catch-finally.js
@@ -0,0 +1,22 @@
+// Finally
+
+try {
+    // É executado quando não há erros
+} catch (e) {
+    // É executado quando há erros
+} finally {
+    // É executado sempre
+}
+
+// É útil para casos onde precisa ser finalizada alguma tratativa, independente de apresentar erro ou não. Ex:
+
+try {
+    console.log('Abrindo meu arquivo');
+    console.log('Arquivo apresentou erro');
+    console.log(e); // como apresentou erro, vai direto ao catch, pulando o fechamento do arquivo
+    console.log('Arquivo fechado');
+} catch (erro) {
+    console.log('Tratando erro');
+} //finally {
+    //console.log('Fechando arquivo') // sempre vai fechar o arquivo, independente se apresentou erro ou não
+    //console.log()
